648d0d1950a109f6c45269c3b3369858305bb3d5
dougout <dougout@192.168.1.110>
2012-12-30 19:40:25 -0600

    added equipment

---
+++ src/main/java/com/metispro/model/reservation/Equipment.java
@@ -0,0 +1,46 @@
+package com.metispro.model.reservation;
+
+public class Equipment {
+	
+	private long id;
+
+	private String type;
+
+	private String name;
+
+	private String description;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
